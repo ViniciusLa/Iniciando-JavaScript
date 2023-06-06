4f38b51510dee5e63b52d1c769eebc9c47b1dafe
unknown <juhh120@hotmail.com>
2023-06-06 11:25:51 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+// Booleanos retornam sempre true ou false
+// const = constante ( não pode ser alterado valor futuramente ).
+// let cadastroAtivado = true;
+// const num1 = 10;
+// const num2 = 10;
+
+// console.log(num1===num2)
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
---
+++ type-string.js
@@ -0,0 +1,49 @@
+// const texto1 = "Olá Mundo";
+// const texto2 = 'Olá Mundo';
+// const senha = "senhaSuperSegura456!";
+// const stringNumero = "456987";
+
+// const citacao = 'Meu nome é ';
+// const meuNome = "Leonardo";
+
+// Concatenação
+
+
+
+// Templaye string ou templaye lateral - uma outra forma de escrever string em javascript
+
+// Exemplos de utilização de códigos para printar icones ou caracteres diferentes.
+
+// const cifrao = '\u0024'
+// const aMaiusculo = '\u0041'
+// const tique = '\u2705'
+// const hiragana = '\u3041'
+
+// console.log(cifrao)
+// console.log(aMaiusculo)
+// console.log(tique)
+// console.log(hiragana)
+
+// Comparação de Strings
+
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+//  Esta comparação vai falhar pelo fato das strings terem diferenças entre letras maiusculas e letras minusculas.
+
+// Uma solução é realizar a seguinte opção:
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// const inputMinusculo = input.toLowerCase();
+
+// console.log(cidade === inputMinusculo); // true
+// Utilizamos uma nova variavel chamada imputMinusculo para dizer que ela terá o valor da variável imput porém com a modificação toLowerCase() que irá deixar todas os caracteres minusculos.
+
+
+
+// Esta nova informação (length) pode ser utilizada para sabermos quantos caracteres uma string contém
+
+// const senha = "minhaSenha123";
+// console.log(senha.length) // 13 caracteres
